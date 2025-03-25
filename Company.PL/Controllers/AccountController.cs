660cdea0a8a6ee68e17c54008d3bf8a8d2aee80f
alamyrayman@outlook.sa <alamyrayman@outlook.sa>
2025-03-25 22:06:34 +0200

    SignUp_SignIn

---
+++ Company.PL/Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using Company.DAL.Models;
+using Company.PL.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Company.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager) 
+        {
+           _userManager = userManager;
+        }
+
+        #region SignUp
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDTOs model)
+        {
+            if (ModelState.IsValid) 
+                {
+
+                var user= await _userManager.FindByNameAsync(model.UserName);
+
+                if(user is null)
+                {
+                    user = await _userManager.FindByEmailAsync(model.Email);
+
+                    if( user is null)
+                    {
+                        // Manual Maping 
+                         user = new AppUser
+                        {
+                            UserName = model.UserName,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            IsAgree = model.IsAgree
+                        };
+
+                        var result = await _userManager.CreateAsync(user, model.Password);
+                        if (result.Succeeded)
+                        {
+                            return RedirectToAction("SignIn");
+                        }
+                        foreach (var error in result.Errors)
+                        {
+
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+
+                ModelState.AddModelError("", "Invalid SinUp !!");
+
+            }
+            return View();
+        }
+
+        #endregion
+
+        #region SignIn
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email); 
+
+                if (user is not null)
+                {
+                    var flag = await _userManager.CheckPasswordAsync(user, model.Password);
+                    if (flag)
+                    {
+                        // SignIn 
+                        return RedirectToAction(nameof(HomeController.Index), "Home");
+                    }
+
+                }
+                ModelState.AddModelError("", "Invalid LogIn !!");
+            }
+
+            return View(model);
+        }
+        #endregion
+
+        #region SignOut
+
+        #endregion
+
+
+    }
+}
